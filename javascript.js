08cc4f956202780634ab9ff0ee0971300a3d5a2e
hereisfahad <fahadjabbar31@gmail.com>
2019-03-11 01:43:25 +0500

    add new item to budget controller ..store in data object

--- javascript.js
+++ javascript.js
@@ -30,43 +30,68 @@ var UIController = (function(){
 
 //BudgetController
 var BudgetController = (function(){
-    var Income = function(id,description,value){
+    //income constructor
+    var Incomes = function(id,description,value){
         this.id = id;
         this.description = description;
         this.value = value;
     }
+    //expenses constructor
     var Expenses = function(id,description,value){
         this.id = id;
         this.description = description;
         this.value = value;
     }
+    //data object to store exp and inc
     var data = {
-        totalsItems:{
-            totalExpenses: [],
-            totalIncomes: []
+        totalItems:{
+            inc: [],
+            exp: []
         },
-        totals{
+        totals: {
             exp: 0,
-            inc:0
+            inc: 0
         }
     }
     
     
+    
     return{
-        
+        //public function to create and add exp or income to  data                
+        addItem: function(type,des,val){
+            var newItem,id;
+            //initalize id
+            if(data.totalItems[type].length>0){
+                id = data.totalItems[type][data.totalItems[type].length-1].id+1;
+            }else{
+                id = 0;  
+            }
+            //create expense object
+            if(type === "exp"){
+                newItem = new Expenses(id,des,val)
+            }else if(type === "inc"){//create income object
+                newItem = new Incomes(id,des,val)
+            }
+            //push the newItem object in the respective array of data
+            data.totalItems[type].push(newItem);
+            return newItem;
+        },
+
     }
 })();
 
 
 
 //GlobalController
-var Controller = (function(bugetCtrl,uiCtrl){
-    
+var Controller = (function(budgetCtrl,uiCtrl){
+    var inputs,newItem;
     //add btn handler
     var ctrlAddBtn = function(){
         //get inputs
-        uiCtrl.getInputs();
-        console.log(uiCtrl.getInputs());       
+        inputs = uiCtrl.getInputs();//1
+//        console.log(uiCtrl.getInputs());  //inputs from UI  
+        newItem = budgetCtrl.addItem(inputs.type,inputs.description,inputs.value);
+        console.log(newItem);//output the newTiem object
     }
     
     var setUpEventListener = function(){
